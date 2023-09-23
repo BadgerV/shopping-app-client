4b2f7eaf24c680b3bd79b2901103db5ca45eb714
BadgerV <segunfaozan@gmail.com>
2023-09-23 13:56:06 +0100

    prettier

--- src/newComponents/Footer/Footer.tsx
+++ src/newComponents/Footer/Footer.tsx
@@ -1,9 +1,48 @@
-import './footer.css';
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="footer_left">
+        <span className="footer-exclusive-text">Exclusive</span>
+        <span className="footer-subscribe-text">Subscribe</span>
+        <span className="footer-discount-text">
+          Get 10% off your first order
+        </span>
+        <input type="text" placeholder="Email" />
+      </div>
+      <div className="footer-middle">
+        <div className="footer-middle_left">
+          <span className="footer-text_header">Support</span>
+          <span className="footer-text_text">
+            111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.
+          </span>
+          <span className="footer-text_text">segunfaozan112@gmail.com</span>
+          <span className="footer-text_text">08078754215</span>
+        </div>
 
-export default Footer
+        <div className="footer-middle_middle">
+          <span className="footer-text_header">Support</span>
+          <span className="footer-text_text">
+            111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.
+          </span>
+          <span className="footer-text_text">segunfaozan112@gmail.com</span>
+          <span className="footer-text_text">08078754215</span>
+        </div>
+        <div className="footer-middle_right">
+          <span className="footer-text_header">Support</span>
+          <span className="footer-text_text">
+            111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.
+          </span>
+          <span className="footer-text_text">segunfaozan112@gmail.com</span>
+          <span className="footer-text_text">08078754215</span>
+        </div>
+      </div>
+      <div className="footer_left">
+        <span className="footer-text_header">Support</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
