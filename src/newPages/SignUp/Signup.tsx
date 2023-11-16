8a949c36ecda77d321d0a6613ee6bc09e1dcee7c
BadgerV <segunfaozan@gmail.com>
2023-11-16 06:27:21 +0100

    prettier

--- src/newPages/SignUp/Signup.tsx
+++ src/newPages/SignUp/Signup.tsx
@@ -14,8 +14,6 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import LoadingComponent from "../../newComponents/LoadingComponent/LoadingComponent";
 
-//just another comment, look awy
-
 const Signup = () => {
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
